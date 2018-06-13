8fa5a60bcb4b00ef335423e572db182064a44768
Student <student@cse.local>
2018-06-12 20:51:24 -0400

    added label

--- gitDemo/Form1.Designer.cs
+++ gitDemo/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.txt_sampleButton = new System.Windows.Forms.Button();
+            this.lblSampleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txt_sampleButton
@@ -40,21 +41,33 @@
             this.txt_sampleButton.Text = "sample button";
             this.txt_sampleButton.UseVisualStyleBackColor = true;
             // 
+            // lblSampleLabel
+            // 
+            this.lblSampleLabel.AutoSize = true;
+            this.lblSampleLabel.Location = new System.Drawing.Point(149, 160);
+            this.lblSampleLabel.Name = "lblSampleLabel";
+            this.lblSampleLabel.Size = new System.Drawing.Size(65, 13);
+            this.lblSampleLabel.TabIndex = 1;
+            this.lblSampleLabel.Text = "sample label";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblSampleLabel);
             this.Controls.Add(this.txt_sampleButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button txt_sampleButton;
+        private System.Windows.Forms.Label lblSampleLabel;
     }
 }
 
